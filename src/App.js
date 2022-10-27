adf7a11945e9283d06f987e240c99f67a00cad88
Erin Baright <erinbaright@Erins-MacBook-Pro.local>
2022-10-27 14:30:44 -0400

    api working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [parks, setParks] = useState();
+
+  const getData = async () => {
+    const response = await axios.get(`https://developer.nps.gov/api/v1/parks?stateCode=CA&api_key=wsocPvzImHobFSy9eqfISnL5LT3UCLNmCavydLOp`, {
+      headers: { Accept: "application/json" },
+    });
+    setParks(response.data.data);
+    console.log(response.data.data)
+  }
+  // console.log(parks[0].fullName);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (!parks) return <h2></h2>
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="parks">
+      <header className="header">
+        <h1>National Parks</h1>
+        <h2>{parks[4].fullName}</h2>
       </header>
+      <span className='pictures'>
+      {}
+      </span>
     </div>
   );
 }
